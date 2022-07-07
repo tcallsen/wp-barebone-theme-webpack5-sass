cd58d2943b7532217a3dbd9090e0515fe6e2d10f
Taylor Callsen <gritto2008@gmail.com>
2022-07-07 14:01:58 -0700

    simplifying compiled asset enqueueing in wp for demo purposes

--- functions.php
+++ functions.php
@@ -1,16 +1,13 @@
 <?php 
 
-  // register webpack stylesheet and js with theme - requires extra logic to 
-  // 	search for and detect webpack versioning hash in filename
+  // register webpack compiled js and css with theme
   function enqueue_webpack_scripts() {
     
     $cssFilePath = glob( get_template_directory() . '/css/build/main.css' );
-    // $cssFilePath = glob( get_template_directory() . '/css/build/main.min.*' );
     $cssFileURI = get_template_directory_uri() . '/css/build/' . basename($cssFilePath[0]);
     wp_enqueue_style( 'site_main_css', $cssFileURI );
     
     $jsFilePath = glob( get_template_directory() . '/js/build/main.js' );
-    // $jsFilePath = glob( get_template_directory() . '/js/build/main.min.*.js' );
     $jsFileURI = get_template_directory_uri() . '/js/build/' . basename($jsFilePath[0]);
     wp_enqueue_script( 'site_main_js', $jsFileURI , null , null , true );
      
